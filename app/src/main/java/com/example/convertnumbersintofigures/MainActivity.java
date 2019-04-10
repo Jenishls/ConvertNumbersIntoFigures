cf28c2ede8d44c609bb3d6dc4e55f468359e3824
jenishShubhu <jlshrestha90@gmail.com>
2019-04-10 17:50:37 +0545

    Created layout and created converter class

--- app/src/main/java/com/example/convertnumbersintofigures/MainActivity.java
+++ app/src/main/java/com/example/convertnumbersintofigures/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.convertnumbersintofigures;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText etNum;
+    private TextView tvOutput;
+    private Button btnConvert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnConvert = findViewById(R.id.btnConvert);
+        etNum= findViewById(R.id.etNum);
+
+        tvOutput = findViewById(R.id.tvOutput);
+
+        btnConvert.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnConvert) {
+            if (validation()) {
+                String numberz =etNum.getText().toString();
+                final long number = Long.parseLong(numberz);
+                String returnz = Converter.convert(number);
+                tvOutput.setText(returnz);
+
+            }
+        }
+    }
+
+    public boolean validation(){
+        boolean flag = true;
+        if (TextUtils.isEmpty(etNum.getText().toString())){
+            etNum.setError("Please enter Height");
+            etNum.requestFocus();
+            flag = false;
+        }
+
+        return flag;
     }
 }
